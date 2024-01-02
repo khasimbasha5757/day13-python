0e32b5f36ba98bde27a37c708954a17f600af9ed
SHAIK KHASIM  BASHA <115825218+khasimbasha5757@users.noreply.github.com>
2024-01-02 20:09:15 +0530

    Add files via upload

---
+++ 13th day.py
@@ -0,0 +1,17 @@
+# for i in range(3):
+          # print(i+1)
+# i=0
+# while(i<10):
+          # print(i+1)
+          # i=i+1
+# print("Done with the loop")
+# a=100
+# if(a==100):
+#         print("True",a)
+# # i=int(input("Give me a number: "))
+# count=5
+# while(count>0):
+#           print(count)
+#           count=count-1
+# else:
+#         print("it has completed")
